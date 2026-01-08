1088a16d89a1dd8c912118da5846c2dcfc18fecf
xiaomi86pro <xiaomi86pro@gmail.com>
2026-01-08 19:53:48 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   // Thay <ten-repo-cua-ban> bằng tên repository trên GitHub của bạn
   // Ví dụ: https://github.com/user/my-project thì base là '/my-project/'
-  base: '/<ten-repo-cua-ban>/', 
+  base: '/<EnglishAdventure>/', 
 })
