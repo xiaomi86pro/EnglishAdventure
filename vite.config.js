c110d8aa2b0542ee81523a208cf70039458732cc
xiaomi86pro <xiaomi86pro@gmail.com>
2026-01-11 17:30:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  // Thay <ten-repo-cua-ban> bằng tên repository trên GitHub của bạn
-  // Ví dụ: https://github.com/user/my-project thì base là '/my-project/'
+  // Base giữ nguyên của bạn
   base: '/EnglishAdventure/', 
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        // Chỉ định tất cả các file HTML bạn có
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html')
+      }
+    }
   }
 })
